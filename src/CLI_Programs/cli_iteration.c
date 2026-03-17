af5419a8270b235a49bfd8b56e195479cf370d5c
Pravin Chavan <pravinac8@gmail.com>
2026-03-17 17:14:18 +0530

    Add loop to print all CLI arguments
    - Iterates through argv using a for loop
    - Prints each command line argument dynamically
    - Helps in understanding argument indexing and traversal

---
+++ src/CLI_Programs/cli_iteration.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("NUmber of cmmd line argument are : %d\n", argc);
+
+    int i = 0;
+
+    for (i = 0; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
+    
+
+    return 0;
+}
