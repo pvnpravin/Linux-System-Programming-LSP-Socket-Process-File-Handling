4cbc4cf458f149f15a60d0a16a3100d0ff5365ae
Pravin Chavan <pravinac8@gmail.com>
2026-03-17 17:11:10 +0530

    Fix argument indexing to correctly use argv[1..4] for inputs

--- src/CLI_Programs/cli_argv_print.c
+++ src/CLI_Programs/cli_argv_print.c
@@ -5,10 +5,10 @@ int main(int argc, char *argv[])
     printf("NUmber of cmmd line argument are : %d\n", argc);
 
     printf("Executable Name : %s", argv[0]);
-    printf("IP Address : %s\n", argv[0]);
-    printf("Port Number : %s\n", argv[0]);
-    printf("Targeted File : %s\n", argv[0]);
-    printf("New File Name : %s", argv[0]);
+    printf("IP Address : %s\n", argv[1]);
+    printf("Port Number : %s\n", argv[2]);
+    printf("Targeted File : %s\n", argv[3]);
+    printf("New File Name : %s", argv[4]);
 
     return 0;
 }
