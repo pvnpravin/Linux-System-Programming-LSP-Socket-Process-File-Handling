ac7035f0c2220b99c8c76acf697eedeffc4d6b4d
Pravin Chavan <pravinac8@gmail.com>
2026-03-17 17:09:37 +0530

    print command-line arguments (cli_argv_print.c)
    Add command line argument parsing template for network/file inputs
    
    - Prints total number of command line arguments (argc)
    - Displays executable name using argv[0]
    - Adds placeholders for:
      - IP Address
      - Port Number
      - Target file name
      - New file name
    - Prepares structure for client/server or file transfer programs

---
+++ src/CLI_Programs/cli_argv_print.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("NUmber of cmmd line argument are : %d\n", argc);
+
+    printf("Executable Name : %s", argv[0]);
+    printf("IP Address : %s\n", argv[0]);
+    printf("Port Number : %s\n", argv[0]);
+    printf("Targeted File : %s\n", argv[0]);
+    printf("New File Name : %s", argv[0]);
+
+    return 0;
+}
